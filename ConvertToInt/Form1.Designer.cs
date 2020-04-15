7f05a298bbf4e59561a1b3a35e661c73b6b64102
JONATAS ALVES <jonatasalves108@uni9.edu.br>
2020-04-15 00:21:37 -0300

    edit

--- ConvertToInt/Form1.Designer.cs
+++ ConvertToInt/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConvertToInt
 {
-    partial class Form1
+    partial class Conversor
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Transparent;
+            this.btnConvert.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnConvert.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnConvert.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnConvert.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnConvert.Location = new System.Drawing.Point(59, 145);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(110, 33);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(80, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Visor";
+            // 
+            // Conversor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 334);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(232, 219);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Conversor";
+            this.Opacity = 0.99D;
+            this.ShowIcon = false;
+            this.Text = "ConvertToInt";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ConvertToInt/Program.cs
+++ ConvertToInt/Program.cs
@@ -16,7 +16,7 @@ namespace ConvertToInt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Conversor());
         }
     }
 }
